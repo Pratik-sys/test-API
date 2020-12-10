fbe44821e9160e3ca4d1818814f874989b1da10d
Pratik-Sys <pithore.pratik@gmail.com>
2020-12-10 08:01:36 +0530

    return the pan details as per the input of pan number
    - for the testing purpose the values are hardcode
    
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
-@api.route('/test')
+@api.route("/test")
 class test(Resource):
     def get(self):
-        return jsonify({
-            "Name": "Pratik Pithore"
-        })
-    
-
-if __name__ == '__main__':
+        return jsonify(
+            {
+                "pan": "ANRPM2537J",
+                "name": "Dinesh Kumar",
+                "dob": "1990-10-25",
+                "father_name": "Hari Kumar",
+                "client_id": "4feb601e-2316-4dda-8d91-28c89cdb2335",
+            }
+        )
+if __name__ == "__main__":
     app.run(debug=True, port=8080)
