37cf36ff27ee78f4ab6d7db1e6ba5161f8194a3a
Pratik-Sys <pithore.pratik@gmail.com>
2020-12-10 07:56:41 +0530

    use flask restx api
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route('/test')
+class test(Resource):
+    def get(self):
+        return jsonify({
+            "Name": "Pratik Pithore"
+        })
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
