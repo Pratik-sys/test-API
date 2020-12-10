17918ae36817125b4151ce76539c09230fffef0a
Pratik-Sys <pithore.pratik@gmail.com>
2020-12-10 14:53:58 +0530

    add validation to route
    - if the entered pan is same as stored in db the details will be returned and if not error will pop.
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- app.py
+++ app.py
@@ -13,27 +13,27 @@ class Details(db.Document):
     dob = db.StringField(required=True)
     father_name = db.StringField(required=True)
 
-add = Details( pan = "ANRPM2537J", name = "Dinesh Kumar", dob ="1990-10-25", father_name="Hari Kumar"
+# add = Details( pan = "ANRPM2537J", name = "Dinesh Kumar", dob ="1990-10-25", father_name="Hari Kumar"
+# )
+# add.save()
+# print("done")
 
-)
-add.save()
-print("done")
-
-@api.route("/test")
+@api.route("/<string:pan_number>")
 class Test(Resource):
-    def get(self):
+    def get(self, pan_number):
          collect = Details.objects()
          for c in collect:
-            return jsonify(
-                {
-                    "pan": c.pan,
-                    "name": c.name,
-                    "dob": c.dob,
-                    "father_name": c.father_name,
-                    "client_id": "4feb601e-2316-4dda-8d91-28c89cdb2335",
-                }
-            )
-
-
+            if pan_number == c.pan :
+                return jsonify(
+                    {
+                        "pan": c.pan,
+                        "name": c.name,
+                        "dob": c.dob,
+                        "father_name": c.father_name,
+                        "client_id": "4feb601e-2316-4dda-8d91-28c89cdb2335",
+                    }
+                )
+            else:
+                return jsonify({'message': 'error occured'})
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
