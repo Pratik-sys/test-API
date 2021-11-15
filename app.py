bad75398cebd7b2ca8fb24516b44fac92fa8330f
Pratik-sys <pithore.pratik@gmail.com>
2021-11-15 13:58:12 +0530

    use black formatting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 from flask_restx import Resource, Api
 from flask_mongoengine import MongoEngine
 from datetime import datetime
@@ -10,12 +10,14 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 api = Api(app)
 load_dotenv(".env")
-app.config["MONGODB_SETTINGS"] = {"host" : os.getenv("DB_URL")}
+app.config["MONGODB_SETTINGS"] = {"host": os.getenv("DB_URL")}
 db = MongoEngine(app)
-app.config['JWT_SECRET_KEY'] = "".join(
-        random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
-        for _ in range(16))
+app.config["JWT_SECRET_KEY"] = "".join(
+    random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+    for _ in range(16)
+)
 jwt = JWTManager(app)
+
 
 class Details(db.Document):
     Name = db.StringField()
@@ -23,6 +25,7 @@ class Details(db.Document):
     PanNumber = db.StringField()
     Dob = db.DateField()
     FatherName = db.StringField()
+
 
 @api.route("/login")
 class LoginUser(Resource):
@@ -32,40 +35,44 @@ class LoginUser(Resource):
             user = Details.objects(Email=record["email"]).first()
             if user.Email and user.PanNumber == record["panNumber"]:
                 gen_token = create_access_token(identity=user.Email)
-            return jsonify({"msg": gen_token},200)
+            return jsonify({"msg": gen_token}, 200)
         except Exception as ex:
             print(ex)
-            return jsonify({"msg": "No such user found"},500)
+            return jsonify({"msg": "No such user found"}, 500)
+
+
 @api.route("/dummydata")
 class DummyData(Resource):
     def post(self):
         record = json.loads(request.data)
         try:
             details = Details(
-                Name = record["name"],
-                Email = record["email"],
-                PanNumber = record["panNumber"],
-                Dob = datetime.now(),
-                FatherName = record["fatherName"]
+                Name=record["name"],
+                Email=record["email"],
+                PanNumber=record["panNumber"],
+                Dob=datetime.now(),
+                FatherName=record["fatherName"],
             )
             details.save()
-            return jsonify({"msg" : "Dummy Data entered!"}, 201)
+            return jsonify({"msg": "Dummy Data entered!"}, 201)
         except Exception as ex:
             print(ex)
-            return jsonify({"msg" : "error while entering data"}, 404)
+            return jsonify({"msg": "error while entering data"}, 404)
+
+
 @api.route("/<string:pan_number>/details")
 class GetData(Resource):
     @jwt_required()
-    def get(self, pan_number:str):
+    def get(self, pan_number: str):
         try:
-            details = Details.objects(PanNumber = pan_number).first()
+            details = Details.objects(PanNumber=pan_number).first()
             if details:
-                return jsonify({"msg": details},201)
-            return jsonify({"msg" : "Invalid Pan Number"},500)
+                return jsonify({"msg": details}, 201)
+            return jsonify({"msg": "Invalid Pan Number"}, 500)
         except Exception as ex:
             print(ex)
-            return jsonify({"msg" : "Something went wrong"}, 404)
+            return jsonify({"msg": "Something went wrong"}, 404)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
