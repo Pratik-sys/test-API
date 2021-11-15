f5984ff288cceb8557535b124e96b22eff5f52b1
Pratik-sys <pithore.pratik@gmail.com>
2021-11-15 12:41:20 +0530

    update requirements and clean up routes

--- app.py
+++ app.py
@@ -3,42 +3,44 @@ from flask_restx import Resource, Api
 from flask_mongoengine import MongoEngine
 from datetime import datetime
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token,get_jwt_identity
+import os, random, string
+from dotenv import load_dotenv
+from models import Details
+
 
 app = Flask(__name__)
 api = Api(app)
-app.config["MONGODB_SETTINGS"] = {"db": "myapp"}
+load_dotenv(".env")
+app.config["MONGODB_SETTINGS"] = {"host" : os.getenv("URI")}
 db = MongoEngine(app)
-app.config['JWT_SECRET_KEY'] = 'secrete-key'  
+app.config['JWT_SECRET_KEY'] = "".join(
+        random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+        for _ in range(16))
 jwt = JWTManager(app)
 
-@app.route('/login', methods=['POST','GET'])
-def login():
-    items = Details.objects()
-    username = request.json.get('username')
-    password = request.json.get('password')
-    for item in items:
-        if username == item.name:
-            access_token = create_access_token(identity=username)
-        else:
-            return jsonify({"msg": "Invalid  user "})
-    return jsonify({"access_token":access_token},200)
+@api.route("/login")
+class LoginUser(Resource):
+    def post():
+        try:
+            pass
+        except:
+            pass
 
+@api.route("/dummydata")
+class DummyData(Resource):
+    def post():
+        try: 
+            pass
+        except: 
+            pass
 @api.route("/<string:pan_number>")
 class GetData(Resource):
     @jwt_required
     def get(self, pan_number):
-        # retrive = Details.objects()
-        for i in retrive:
-            if pan_number != i.pan:
-                return jsonify({"msg": "Error, no such pan in the database"},403)
-            else:
-                return jsonify({
-                    "pan": i.pan,
-                    "name": i.name,
-                    "dob":i.dob.strftime("%Y-%m-%d"),
-                    "father_name": i.father_name,
-                    "client_id": i.client_id
-                }, 201)
+        try:
+            pass
+        except:
+            pass
 
 
 if __name__ == "__main__":
